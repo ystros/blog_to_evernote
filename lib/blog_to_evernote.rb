2a0a752656e3c4eb2edec411856946e6b2bfa1e4
Brian Upton <ystros@gmail.com>
2015-09-16 01:07:40 -0700

    Connect the post importer to the Evernote client.

--- lib/blog_to_evernote.rb
+++ lib/blog_to_evernote.rb
@@ -1,2 +1,8 @@
 require 'rubygems'
 require 'bundler/setup'
+Bundler.setup(:default)
+
+require_relative 'blog_to_evernote/importer'
+
+importer = BlogToEvernote::Importer.new
+importer.import
--- lib/blog_to_evernote/importer.rb
+++ lib/blog_to_evernote/importer.rb
@@ -1,15 +1,32 @@
 require 'mysql2'
 require_relative 'config'
+require_relative 'evernote_sanitizer'
+require_relative 'evernote_client'
 
 module BlogToEvernote
+  # Loads posts from the configured database and adds them as notes to Evernote.
   class Importer
-    def initialize
-      config = Config.load_file
+    def initialize(config = Config.load_file)
       @structure = config.blog_structure
       @client = create_db_client(config.database_connection)
+      @evernote = EvernoteClient.new(config.evernote, EvernoteSanitizer.new(config.blog_structure.base_url))
     end
 
     def import
+      @evernote.request_oauth_token
+      puts "Importing: "
+      imported_posts = posts_for_import.map do |post|
+        result = @evernote.create_note_from_post(post)
+        putc result[0] ? "." : "x"
+        result
+      end
+      puts "\n\n"
+
+      succeeded, failed = imported_posts.partition { |result| result[0] }
+      puts "Successfully imported posts: #{succeeded.length}"
+      failed.each do |result|
+        puts "Unable to import post ID #{result[1].id} due to error: #{result[2].inspect}"
+      end
     end
 
     def posts_for_import
